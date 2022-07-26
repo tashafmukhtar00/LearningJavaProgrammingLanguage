af6f074812136f8f22a58bfc9deed2b4448b6b2f
tashaf <tashafmukhtar@gmail.com>
2022-07-26 14:00:21 +0400

    adding java topics to be covered in this learning path

---
+++ src/fundamentals/HelloWorldJava.java
@@ -0,0 +1,15 @@
+package fundamentals;
+
+public class HelloWorldJava {
+    public static void main(String[] args) {
+
+        System.out.print("Hello Welcome to the world of java programming language ");
+        System.out.print(",Here we learn \n");
+        System.out.print("Java language fundamentals\n");
+        System.out.println("Object Oriented Programming in Java");
+        System.out.println("Java Collection Framework");
+        System.out.println("Advance Java");
+
+        
+    }
+}
